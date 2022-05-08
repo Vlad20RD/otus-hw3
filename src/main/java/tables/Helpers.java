c2ae2377acf74a2de2de17975544701646c1e070
Vlad20RD <vladcska35topppchik@gmail.com>
2022-05-08 08:46:16 +0300

    update

--- src/main/java/tables/Helpers.java
+++
@@ -1,7 +0,0 @@
-package tables;
-
-public class Helpers extends TableAbs{
-    public Helpers(String dbType) {
-        super(dbType);
-    }
-}
--- src/main/java/tables/Request.java
+++
@@ -1,7 +0,0 @@
-package tables;
-
-public class Request extends TableAbs{
-    public Request(String dbType) {
-        super(dbType);
-    }
-}
